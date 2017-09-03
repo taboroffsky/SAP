bf82503ea1d8c127dd893849d316d2382099d486
Andrii Taborovskyi <taboroffsky6@gmail.com>
2017-09-03 13:56:44 +0300

    Added (1)

---
+++ One_More_SPA_Project/Scripts/script.js
@@ -0,0 +1,45 @@
+﻿$(document).ready(function () {
+
+    $(document).on('scroll', scroller);
+
+    $('a[href^="#"]').on('click', function (e) {
+
+        e.preventDefault();
+        $(document).off('scroll');
+
+        $('a').each(function () {
+            $(this).removeClass('active');
+        });
+
+        $(this).addClass('active');
+
+        var moveTo = this.hash;
+
+        $('html,body').stop().animate({ 'scrollTop': $(this.hash).offset().top }, 500, 'swing', function () {
+            window.location.hash = moveTo;
+            $(document).on('scroll', scroller);            
+        });
+                
+        
+       
+    });
+});
+
+
+function scroller(e) {
+    var scrollerPosition = $(document).scrollTop();
+
+    $('.navbutton a').each(function () {
+
+        var currentLink = $(this);
+        var currentAttr = $(this).attr('href');
+
+        if ($(currentAttr).position().top <= scrollerPosition && $(currentAttr).position().top + $(currentAttr).height() > scrollerPosition) {
+            currentLink.addClass('active');
+            window.loca.hash = currentAttr;
+        }
+        else
+            currentLink.removeClass('active');
+
+    })
+};
